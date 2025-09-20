bb6717c1687a711b1598be2a4c4e709155ff63d1
febekeyt <polesticophoebe@gmail.com>
2025-09-21 01:50:05 +0800

    Counter App

---
+++ app/Activity5(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from 'react-native';
+import CounterApp from './CounterApp';
+
+export default function App() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <CounterApp/>
+    </SafeAreaView>
+  );
+}
